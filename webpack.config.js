98311c3e5cfb1c4537efa23274b3efd266435638
Indiel <kori.yanagi@gmail.com>
2019-12-05 18:04:51 +0100

    fix: errors in the implementation of some elements were taken into account, video was added

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ let conf = {
     output: {
         path: path.resolve(__dirname, './dist/'),
         filename: '[name].js',
-        publicPath: '/'
+        publicPath: ''
     },
     module: {
         rules: [
